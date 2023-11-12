e204816080989e47cf8ff6ab986f9e2b7cab8e1b
Jonathan Schwender <schwenderjonathan@gmail.com>
2023-11-12 11:55:19 +0100

    Add alternative parsing implementation
    Add a simplistic handwritten alternativ to the
    regex implementation.
    This could be used with less dependencies
    and might have a lower performance cost with larger
    files, since the file is only read once.

--- src/compiler_depend.rs
+++ src/compiler_depend.rs
@@ -10,7 +10,82 @@ pub(crate) struct DepInfo<'h> {
 
 #[derive(Debug)]
 pub(crate) enum ParseError {
+    UnexpectedEOF,
+    UnrecognizedLine,
+    UnexpectedColon,
+}
 
+// non-regex based parsing
+fn extract_dependencies2(input: &str) -> Result<Vec<DepInfo>, ParseError> {
+    let mut lines = input.lines();
+    let mut dep_info = vec![];
+    // `Some` if we have state due to line continuation on the previous iteration.
+    let mut state: Option<DepInfo> = None;
+    while let Some(line) = lines.next() {
+        let mut finished_line_continuations: bool = false;
+        match state {
+            None => {
+                // Check if line is a comment
+                if line.starts_with('#') {
+                    continue
+                }
+                if line.is_empty() {
+                    continue
+                }
+                let (src, dep1) = line.split_once(':').ok_or(ParseError::UnrecognizedLine)?;
+                if dep1.contains(':') {
+                    return Err(ParseError::UnexpectedColon);
+                }
+                match dep1.strip_suffix('\\') {
+                    Some(stripped) => {
+                        let trimmed = stripped.trim();
+                        let mut deps = vec![];
+                        if !trimmed.is_empty() {
+                            deps.push(trimmed);
+                        }
+                        state = Some(DepInfo {
+                            object: src,
+                            deps,
+                        });
+                    },
+                    None => {
+                        let trimmed = dep1.trim();
+                        let mut deps = vec![];
+                        if !trimmed.is_empty() {
+                            deps.push(trimmed);
+                        }
+                        dep_info.push(DepInfo {
+                            object: src,
+                            deps,
+                        })
+                    }
+                }
+            },
+            Some(ref mut info ) => {
+                if line.contains(':') {
+                    return Err(ParseError::UnexpectedColon);
+                }
+                let new_dep = match line.strip_suffix('\\') {
+                    Some(stripped) => {
+                        stripped.trim()
+                    },
+                    None => {
+                        finished_line_continuations = true;
+                        line.trim()
+                    }
+                };
+                info.deps.push(new_dep);
+            }
+        }
+        if finished_line_continuations {
+            dep_info.push(state.expect("Expected to have DepInfo"));
+            state = None;
+        }
+    }
+    if state.is_some() {
+        return Err(ParseError::UnexpectedEOF)
+    }
+    Ok(dep_info)
 }
 
 pub(crate) fn extract_dependencies(input: &str) -> Result<Vec<DepInfo>, ParseError> {
@@ -59,6 +134,29 @@ src/utils/CMakeFiles/utils.dir/blah.c.o: blah/blah.h
                 "/home/jschwender/Dev/cmake_get_deps/tests/test_cmake_project/src/utils/include/test_project/utils/utils.h",
                 "/usr/include/bits/libc-header-start.h",
                 "/usr/lib/gcc/x86_64-redhat-linux/13/include/stdint.h",
+            ]
+        };
+        let expected_blah = DepInfo {
+            object: "src/utils/CMakeFiles/utils.dir/blah.c.o",
+            deps: vec!["blah/blah.h"],
+        };
+        let expected_sys = DepInfo {
+            object: "/usr/lib/gcc/x86_64-redhat-linux/13/include/stdint.h",
+            deps: vec![]
+        };
+        let expected = vec![expected_utils, expected_blah, expected_sys];
+        assert_eq!(depinfo, expected);
+    }
+
+    #[test]
+    fn extract_dependencies2_test() {
+        let depinfo = super::extract_dependencies2(EXAMPLE_FILE).expect("Failed");
+        let expected_utils = DepInfo {
+            object: "src/utils/CMakeFiles/utils.dir/utils.c.o",
+            deps: vec!["/home/jschwender/Dev/cmake_get_deps/tests/test_cmake_project/src/utils/utils.c",
+                       "/home/jschwender/Dev/cmake_get_deps/tests/test_cmake_project/src/utils/include/test_project/utils/utils.h",
+                       "/usr/include/bits/libc-header-start.h",
+                       "/usr/lib/gcc/x86_64-redhat-linux/13/include/stdint.h",
             ]
         };
         let expected_blah = DepInfo {
